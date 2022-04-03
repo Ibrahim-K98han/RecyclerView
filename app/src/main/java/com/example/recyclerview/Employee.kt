7bb085ac89b99dd87204a6895ebf255aa1569d04
unknown <aitvetibrahim@gmail.com>
2022-04-03 09:43:38 +0600

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/Employee.kt
@@ -0,0 +1,22 @@
+package com.example.recyclerview
+
+data class Employee(
+    val id:Int,
+    val name:String,
+    val designation:String
+)
+val employeeList = mutableListOf(
+    Employee(1,"Ibrahim","Developer"),
+    Employee(2,"Hasan","CSE"),
+    Employee(3,"Tomal","EEE"),
+    Employee(4,"Hanif","Desingner"),
+    Employee(5,"Rakib","Developer"),
+    Employee(6,"Hafis","Developer"),
+    Employee(7,"ABC","Developer"),
+    Employee(8,"DEF","Developer"),
+    Employee(9,"GHI","Developer"),
+    Employee(10,"JKL","Developer"),
+    Employee(11,"TUL","Developer"),
+    Employee(12,"HRI","Developer"),
+    Employee(13,"OKK","Developer"),
+)
---
+++ app/src/main/java/com/example/recyclerview/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.recyclerview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerview.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding:FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        val adapter = EmployeeAdapter()
+        binding.emplyoueeRecycler.layoutManager = LinearLayoutManager(activity)
+        binding.emplyoueeRecycler.adapter = adapter
+        adapter.submitList(employeeList)
+        return binding.root
+    }
+
+}
